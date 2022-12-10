f17bd6df807d0fb9383a7f8e85fc565dd38c3acb
Michael Brown <michael@juneteenthconf.com>
2022-12-10 10:23:28 -0800

    read multiple ping requests from same connection

--- app/server.go
+++ app/server.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
@@ -10,8 +13,6 @@ import (
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
@@ -22,7 +23,22 @@ func main() {
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
-	} else {
-		conn.Write([]byte("+PONG\r\n"))
+	}
+	defer conn.Close()
+	fmt.Println("Connection accepted, responding to ping")
+	r := bufio.NewReader(conn)
+	for {
+		_, err := r.ReadBytes(byte('\n'))
+		switch err {
+		case nil:
+			break
+		case io.EOF:
+		default:
+			fmt.Println("ERROR", err)
+		}
+		_, err = conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			fmt.Println("Error writing response: ", err.Error())
+		}
 	}
 }
