955837a56e58e0c18ff6b8b6876e0ea123ac9433
Michael Brown <michael@juneteenthconf.com>
2022-12-10 11:50:59 -0800

    added debug info

--- app/server.go
+++ app/server.go
@@ -25,13 +25,14 @@ func main() {
 		os.Exit(1)
 	}
 	defer conn.Close()
-	fmt.Println("Connection accepted, responding to ping")
+	fmt.Println("Connection accepted...waiting for ping")
 	r := bufio.NewReader(conn)
 	w := bufio.NewWriter(conn)
 	for {
 		_, err := r.ReadBytes(byte('\n'))
 		switch err {
 		case nil:
+			fmt.Println("Ping received, sending response")
 			break
 		case io.EOF:
 		default:
@@ -40,6 +41,7 @@ func main() {
 		if _, err := w.WriteString("+PONG\r\n"); err != nil {
 			fmt.Println("Error writing response: ", err.Error())
 		}
+		fmt.Println("Flushing the buffer")
 		w.Flush()
 	}
 }
